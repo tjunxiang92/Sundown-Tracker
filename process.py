1acb1706cd48961b7e1e9a1ccb5e076102d34b8a
Tan Jun Xiang <tjunxiang92@gmail.com>
2016-05-31 15:33:00 +0800

    Updated with Error Threshold

--- process.py
+++ process.py
@@ -1,15 +1,19 @@
 from burp import *
 import time, os
 
+## Parameters
 start_range = 20001
 folder		= 'records21'
 total		= 20000
+
+error_threshold = 3
 skip_errors = [1003]
+
+
 c = """
 POST /SundownSrv/1xcessResult HTTP/1.1
-Cookie: JSESSIONID=63FD1BCDB0DB6184606A7B63FE98C5F5
 Content-Type: application/x-www-form-urlencoded; charset=UTF-8
-User-Agent: Dalvik/2.1.0 (Linux; U; Android 6.0.1; MI NOTE LTE MIUI/6.4.14)
+User-Agent: Dalvik/2.1.0
 Host: ec2-52-34-46-127.us-west-2.compute.amazonaws.com
 Connection: close
 Accept-Encoding: gzip
@@ -20,25 +24,36 @@ no=%s&
 
 end_range = start_range + total
 try:
+	# Searches for the last record saved on the server
 	start_range = int(os.listdir('records21')[-1][:-4]) + 1
 except:
 	pass
 
+# Loop through all the range
 for i in range(start_range, end_range):
 	## Sleep for a while
 	if i % 10 == 0:
 		print '%d%% Done. %d/%d' % (((i - start_range) * 100) / total, (i - start_range), end_range - start_range)
 		time.sleep(3)
 
+	# Fetch some requests
 	r = code('http', c % (i))
-
 	rjson = r.json()
+
+	# An error occured
 	if not rjson[u'result'] == 0:
 		if rjson[u'result'] in skip_errors:
 			continue
-		print 'Error: %d, Code: %d' % (rjson[u'result'], i)
+
+		# If Error above a certain threshold then quit application		
+		error_threshold = error_threshold - 1
+		if error_threshold > 0:
+			print 'Error: %d, Code: %d' % (rjson[u'result'], i)
+			continue
+
 		exit()
 
+	# Save to a file
 	with open('%s/%d.txt' % (folder, i), 'w') as f:
 		f.write(r.text.encode('ascii', 'ignore').decode('ascii'))
 
